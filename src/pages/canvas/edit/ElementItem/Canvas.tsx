f3d14c3543d681d3dfef35faaa2df5fa796493ae
Ahuang0107 <Seto1920748235@126.com>
2021-05-21 01:58:28 +0800

    A [画布的拖拽(为了看到画布不同地方)]

--- src/pages/canvas/edit/ElementItem/Canvas.tsx
+++ src/pages/canvas/edit/ElementItem/Canvas.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Rnd} from "react-rnd";
 import {Element, Property} from "../../../../interface/entity";
 import CustomComponent from "./CustomComponent";
@@ -11,6 +11,24 @@ export default function Canvas(props: {
     updateElementNode: (data: Element) => void
 }): JSX.Element {
     const {focusElement, setFocusElement, updateElementNode} = props
+    const [scale, setScale] = useState({
+        x: 0,
+        y: 0
+    })
+    const [disableDragging, setDisableDragging] = useState(true)
+    const [enableResizing] = useState(false)
+
+    document.addEventListener('keydown', function (e) {
+        if (e.code === 'Space') {
+            setDisableDragging(false)
+        }
+    })
+    document.addEventListener('keyup', function (e) {
+        if (e.code === 'Space') {
+            setDisableDragging(true)
+        }
+    })
+
     const style = {
         display: "flex",
         alignItems: "center",
@@ -27,8 +45,12 @@ export default function Canvas(props: {
     return (
         <Rnd style={style}
              size={{width: props.property?.width ?? 0, height: props.property?.height ?? 0}}
-             disableDragging={true}
-             enableResizing={false}
+             position={{x: scale.x, y: scale.y}}
+             onDragStop={(e, d) => {
+                 setScale({...scale, x: d.lastX, y: d.lastY});
+             }}
+             disableDragging={disableDragging}
+             enableResizing={enableResizing}
         >
             {elementList}
         </Rnd>
